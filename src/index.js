62a977bc4df11c7ac7e44b4bd14e515906a17deb
Supratik <rikc.2000@gmail.com>
2021-03-05 11:49:38 +0530

    redux to product list

--- src/index.js
+++ src/index.js
@@ -4,11 +4,16 @@ import './index.css';
 import "./assets/main.css";
 import App from './App';
 
+import {Provider} from "react-redux";
+import store from "./store.js"
+
 import '@fortawesome/fontawesome-free/css/all.min.css';
 import 'bootstrap-css-only/css/bootstrap.min.css';
 import 'mdbreact/dist/css/mdb.css';
 
 ReactDOM.render(
-    <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById('root')
 );
